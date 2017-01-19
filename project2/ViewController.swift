5f9315e6cd6ffc9554a21da05a9cb1ba1520a23a
Apple Inc <aigar@4d6a-fee3-8aff-dacb-9d80-87fc-07d0-2001.dyn.estpak.ee>
2017-01-19 22:15:30 +0200

    "guess flags" done

--- project2/ViewController.swift
+++ project2/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Aigar on 19/01/17.
 //  Copyright © 2017 Aigar. All rights reserved.
 //
-
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     
@@ -31,15 +32,37 @@ class ViewController: UIViewController {
             button.layer.borderColor = UIColor.lightGray.cgColor
         }
     
-    askQuestion()
+    askQuestion(action: nil)
+        
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
+        
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        
+        title = countries[correctAnswer].uppercased()
+        
     }
     
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+     let ac = UIAlertController(title: title, message: "Your Score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
